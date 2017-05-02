92693a7815d6e4e02e341d1f54d42b3f0f6d4385
wtffqbpl <renyj@buaa.edu.cn>
2017-05-03 00:42:32 +0800

    Example 3-3

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@
 # coding:utf-8
 # Author: Yuanjun Ren
 
-from flask import Flask
+from flask import Flask, render_template
 from flask_script import Manager
 
 app = Flask(__name__)
@@ -11,12 +11,12 @@ manager = Manager(app)
 
 @app.route("/")
 def index():
-    return "<h1>Hello world!</h1>"
+    return render_template("index.html")
 
 
 @app.route("/user/<name>")
 def user(name):
-    return "<h1>Hello, %s!</h1>" % name
+    return render_template("user.html", name=name)
 
 
 if __name__ == "__main__":

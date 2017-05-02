533b935e9eaacfa374ec7aad38b8862dc65976af
wtffqbpl <renyj@buaa.edu.cn>
2017-05-03 00:31:29 +0800

    hello world version

---
+++ hello.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding:utf-8
+# Author: Yuanjun Ren
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "<h1>Hello world!</h1>"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
